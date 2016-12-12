bb9a5ae507d9d4837bee77a5ea94d8821da43bee
Andy <644879054@qq.com>
2016-12-12 12:53:13 +0800

    heater

---
+++ python/Heaters.py
@@ -0,0 +1,21 @@
+# binary search answer
+
+class Solution(object):
+	
+    def findRadius(self, houses, heaters):
+        """
+        :type houses: List[int]
+        :type heaters: List[int]
+        :rtype: int
+        """
+        heaters = sorted(heaters) + [float('inf')]
+        i = r = 0
+        for x in sorted(houses):
+            while x >= sum(heaters[i:i+2]) / 2.:
+                i += 1
+            r = max(r, abs(heaters[i] - x))
+        return r
+        
+houses = [1,2,3,4]
+heaters = [1,4]
+print Solution().findRadius(houses, heaters)
