6b0c8c2dc049466de9a5456800743dc01f2c6b0b
unknown <songzy.thu@qq.com>
2017-08-20 18:55:10 +0800

    .

--- python/Strange Printer.py
+++ python/Strange Printer.py
@@ -1,30 +1,41 @@
 class Solution(object):
-    def __init__(self):
-        length = 105
-        self.dp = [[[-1 for i in range(length)] for j in range(length)] for k in range(length)]
     def strangePrinter(self, s):
         """
         :type s: str
         :rtype: int
         """
-        dp = self.dp
-        def get_dp(i, j, k):
-            # index from i to j, with k same color with s[j] to the right
+        dp = {} # no need to use lists
+
+        def get_dp(i, j): # use as less state as possible
             if i > j:
                 return 0
-            if dp[i][j][k] != -1:
-                return dp[i][j][k]
-            while j > 0 and s[j-1] == s[j]:
-                j -= 1
-                k += 1
-            #print i, j, k
-            ans = get_dp(i, j-1, 0) + 1 # the first brush is from j to j
-            for t in range(i, j):
-                if s[t] == s[j]: # the fitst brush is from j to t
-                    ans = min(ans, get_dp(i, t, k+1) + get_dp(t+1, j-1, 0))
-            dp[i][j][k] = ans
+            if (i, j) in dp:
+                return dp[i, j]
+            ans = get_dp(i, j-1) + 1
+            for k in range(i, j):
+                if s[k] == s[j]:
+                    ans = min(ans, get_dp(i, k-1) + get_dp(k+1, j))
+            dp[i, j] = ans
             return ans
-        return get_dp(0, len(s)-1, 0)
+        
+##        def get_dp(i, j, k):
+##            # index from i to j, with k same color with s[j] to the right
+##            if i > j:
+##                return 0
+##            if (i, j, k) in dp:
+##                return dp[i, j, k]
+##            while j > 0 and s[j-1] == s[j]:
+##                j -= 1
+##                k += 1
+##            #print i, j, k
+##            ans = get_dp(i, j-1, 0) + 1 # the first brush is from j to j
+##            for t in range(i, j):
+##                if s[t] == s[j]: # the fitst brush is from j to t
+##                    ans = min(ans, get_dp(i, t, k+1) + get_dp(t+1, j-1, 0))
+##            dp[i, j, k] = ans
+##            return ans
+        return get_dp(0, len(s)-1)
 
 s = "ydfatmztyqgmuxjedlxcaftgflhuqldooiqjxqfvinjcksgqeguglnosavorgrhxcaizsnwabfcnalfgrzmepaypxniegsdisljk"
+s = "cbcbddaccadbdcccaddcccccacdcabddaabacccbdaabcb"
 print Solution().strangePrinter(s)
--- python/ZumaGame.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def findMinStep(self, board, hand):
-        """
-        :type board: str
-        :type hand: str
-        :rtype: int
-        """
-        
-# totally no idea
