40b405a5adfcfe3f50dbabd76bf44d7a1751fb7a
songzy <songzy.thu@qq.com>
2018-07-15 10:48:36 +0800

    Sun Jul 15 10:48:36 CST 2018

--- python/Prime Palindrome.py
+++ python/Prime Palindrome.py
@@ -25,18 +25,20 @@ class Solution(object):
                     return temp
 
                 head = str(int(head) + 1)
-                temp = int(head + head[::-1])
-                return temp
+                temp0 = int(head + head[::-1])
+                temp1 = int(10**len(str_n) + 1)
+                return min(temp0, temp1)
             head = str_n[:len(str_n) / 2 + 1]
             temp = int(head + head[:-1][::-1])
             if temp > n:
                 return temp
-            head = str(int(head) + 1)
-            temp = int(head + head[:-1][::-1])
-            return temp
+            head = str(int(head) + 1)  # NOTE: this will cause carry 1
+            temp0 = int(head + head[:-1][::-1])  # 9 -> 101
+            temp1 = int(10**len(str_n) + 1)  # 9 -> 11
+            return min(temp0, temp1)
 
         t = N
-        while not isPrime(t) or not isPalindrome(t): # NOTE: here we need to check both
+        while not isPrime(t) or not isPalindrome(t):  # NOTE: here we need to check both
             t = nextPalindrome(t)
         return t
 
