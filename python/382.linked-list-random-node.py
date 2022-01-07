4174f607c2ef669bf375a676fc3d101dacb36371
Zhengyang Song <songzy_thu@163.com>
2022-01-07 22:03:37 +0800

    Reservoir_sampling

--- python/382.linked-list-random-node.py
+++ python/382.linked-list-random-node.py
@@ -1,11 +1,16 @@
+# https://en.wikipedia.org/wiki/Reservoir_sampling
 # Given a singly linked list, return a random node's value from the linked list.
 # Each node must have the same probability of being chosen.
 
-# Definition for singly-linked list.
+import random
+
+
 class ListNode(object):
+    # Definition for singly-linked list.
     def __init__(self, x):
         self.val = x
         self.next = None
+
 
 class Solution(object):
 
@@ -15,22 +20,29 @@ class Solution(object):
         Note that the head is guaranteed to be not null, so it contains at least one node.
         :type head: ListNode
         """
-        self.head = head        
+        self.head = head
 
     def getRandom(self):
         """
         Returns a random node's value.
         :rtype: int
         """
-        result = None
         current = self.head
+        result_size = 1
+        result = []
         n = 1
-        while current:
-            if random.randint(0, n - 1) == 0:
-                result = current
+        while len(result) < result_size:
+            result.append(current)
             current = current.next
             n += 1
-        return result.val
+
+        while current:
+            index = random.randrange(0, n)
+            if index < result_size:
+                result[index] = current
+            current = current.next
+            n += 1
+        return result[0].val
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(head)
