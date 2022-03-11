29e047efea80fdfdc6ae647f34615c498ee96ad4
Zhengyang Song <songzy_thu@163.com>
2022-03-11 13:57:10 +0800

    dfs for circle detection

--- python/2127.maximum-employees-to-be-invited-to-a-meeting.py
+++ python/2127.maximum-employees-to-be-invited-to-a-meeting.py
@@ -4,108 +4,70 @@ from collections import defaultdict
 
 
 class Solution:
+    def dfs1(self, node, r_favorite, visited):
+        # run dfs from current node
+        # check each of the nodes points to the curent node
+        # find the longest path to this node
+        depth = 1
+        for v in r_favorite[node]:
+            if v in visited:
+                continue
+            visited[v] = True
+            depth = max(depth, 1+self.dfs1(v, r_favorite, visited))
+        return depth
+
+    def dfs2(self, node, favorite, visited):
+        # starting from arbirary node, until we found a node that is already visited.
+        if node in visited:
+            return node, 0, False
+        visited[node] = True
+        visited_node, depth, cycle_detected = self.dfs2(
+            favorite[node], favorite, visited)
+        if visited_node == node:
+            return visited_node, depth+1, True
+        if cycle_detected:
+            return visited_node, depth, True
+        return visited_node, depth+1, False
+
     # each node can appear in at most one cycle or a path points to a cycle.
     # the answer would be the maximum of
-    # 1. length of the largest cycle.
-    # 2. sum of length of pair circles with arms
+    # 1. sum of length of pair circles with arms
+    # 2. length of the largest cycle.
+
     def maximumInvitations(self, favorite):
-        degree = {k: 0 for k in range(len(favorite))}
-        for person in favorite:
-            degree[person] += 1
+        r_favorite = defaultdict(list)
+        for u, v in enumerate(favorite):
+            r_favorite[v].append(u)
 
-        # the next circle id we would use
-        circle_cnt = 0
-        # map each node to the circle id it belongs to
-        circle_id = {}
-        # map each circle to its circle length
-        circle_len = {}
-        # map each circle to the length of the longest path connected
-        circle_path_len_max = {}
+        visited = {}
 
-        while len(degree):
-            # find the head which has no parent
-            # this would be used as the start of next path
-            head = None
-            cur_group = []
+        # case 1
+        # all the edges in the arms are pointing towards the pair nodes.
+        # handling each pair nodes, we are handling n nodes and n edges.
+        # for this case, we do not need to care about the favorite relationship.
+        ans_1 = 0
+        for u in range(len(favorite)):
+            if u in visited:
+                continue
+            if favorite[favorite[u]] == u:
+                visited[u] = True
+                visited[favorite[u]] = True
+                ans_1 += self.dfs1(u, r_favorite, visited)
+                ans_1 += self.dfs1(favorite[u], r_favorite, visited)
 
-            for node in degree:
-                if degree[node] == 0:
-                    head = node
-                    cur_group.append(head)
+        # case 2
+        # for this case, we do not need to care about the reverse favorite relationship.
+        ans_2 = 0
+        for u in range(len(favorite)):
+            if u in visited:
+                continue
+            visited_node, depth, cycle_detected = self.dfs2(
+                u, favorite, visited)
+            if cycle_detected:
+                ans_2 = max(ans_2, depth)
 
-                    degree.pop(head)
-                    degree[favorite[head]] -= 1
-                    break
-            if head == None:
-                break
-
-            # find the path / circlr starting from head
-            visited = {head: True}
-            while favorite[head] not in visited and \
-                    favorite[head] not in circle_id:
-                head = favorite[head]
-                visited[head] = True
-                cur_group.append(head)
-                if degree[head] == 0:
-                    degree.pop(head)
-                    degree[favorite[head]] -= 1
-
-            # favorite[head] is the connection point of path and circle
-            if favorite[head] not in circle_id:
-                # we found a new circle
-                pos = cur_group.index(favorite[head])
-                circle_len[circle_cnt] = len(cur_group) - pos
-                circle_path_len_max[circle_cnt] = defaultdict(int)
-                circle_path_len_max[circle_cnt][favorite[head]] = pos
-                for node in cur_group[pos:]:
-                    circle_id[node] = circle_cnt
-                circle_cnt += 1
-            else:
-                # we found a known circle
-                cur_circle_path = len(cur_group)
-                cur_circle_id = circle_id[favorite[head]]
-                circle_path_len_max[cur_circle_id][favorite[head]] = max(
-                    cur_circle_path, circle_path_len_max[cur_circle_id][favorite[head]])
-
-        # print(circle_cnt)
-        # print(circle_len)
-        # print(circle_path_len_max)
-
-        # From now on, there is no node with in-degree 0.
-        while len(degree):
-            for head in degree.keys():
-                break
-            visited = {head: True}
-            degree.pop(head)
-            cur_group = [head]
-            while favorite[head] not in visited:
-                head = favorite[head]
-                visited[head] = True
-                degree.pop(head)
-                cur_group.append(head)
-            if favorite[head] not in circle_id:
-                pos = cur_group.index(favorite[head])
-                circle_len[circle_cnt] = len(cur_group) - pos
-                circle_path_len_max[circle_cnt] = defaultdict(int)
-                circle_path_len_max[circle_cnt][favorite[head]] = pos
-                for node in cur_group[pos:]:
-                    circle_id[node] = circle_cnt
-                circle_cnt += 1
-
-        # then let us compute the answer candidates
-        # 1. sum of length of pair circles with arms
-        # 2. the length of longest non-pair circles
-        pair_circles_with_arms = 0
-        longest_circle = 0
-        for circle in range(circle_cnt):
-            if circle_len[circle] == 2:
-                pair_circles_with_arms += sum(
-                    circle_path_len_max[circle].values()) + 2
-            else:
-                longest_circle = max(longest_circle,   circle_len[circle])
-
-        # print(longest_circle, pair_circles_with_arms)
-        return max(longest_circle, pair_circles_with_arms)
+        # the answer would be the max of case 1 and case 2
+        return max(ans_1, ans_2)
 
 
 favorite = [1, 0, 0, 2, 1, 4, 7, 8, 9, 6, 7, 10, 8]
