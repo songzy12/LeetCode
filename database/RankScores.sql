bfab27d52e09b284c7b49049e6a016b52bafcecf
Zhengyang Song <songzy_thu@163.com>
2025-01-05 18:59:39 +0800

    Delete folder database and shell.

--- database/RankScores.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  Score,
-  (SELECT count(distinct Score) FROM Scores WHERE Score >= s.Score) Rank
-FROM Scores s
-ORDER BY Score desc
--- shell/TenthLine.sh
+++
@@ -1,6 +0,0 @@
-N=$(cat file.txt|wc -l)
-if [ $N -gt 9 ]; then
-    head file.txt | tail -n 1
-else
-    echo
-fi
